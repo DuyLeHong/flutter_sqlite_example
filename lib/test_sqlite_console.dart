aca6bd9727cf9e5c271d3de717f39ab01cc381eb
Duy Le <lehongduy.bk@gmail.com>
2022-07-28 19:57:28 +0700

    update test_sqlite_console

--- lib/test_sqlite_console.dart
+++ lib/test_sqlite_console.dart
@@ -6,6 +6,8 @@ import 'package:sqflite/sqflite.dart';
 
 import 'dog_model.dart';
 
+late Future<Database> database;
+
 void main() async {
   // Avoid errors caused by flutter upgrade.
   // Importing 'package:flutter/widgets.dart' is required.
@@ -28,74 +30,11 @@ void main() async {
   //   version: 1,
   // );
 
-  final database = openDatabase(join(await getDatabasesPath(), 'doggie_database2.db'), onCreate: (_db, version) {
+  database = openDatabase(join(await getDatabasesPath(), 'doggie_database3.db'), onCreate: (_db, version) {
     return _db.execute(
-      'CREATE TABLE dogs(id INTEGER PRIMARY KEY, name TEXT, age INTEGER)',
+      'CREATE TABLE dogs(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, age INTEGER)',
     );
-  });
-
-  // Define a function that inserts dogs into the database
-  Future<void> insertDog(Dog dog) async {
-    // Get a reference to the database.
-    final db = await database;
-
-    // Insert the Dog into the correct table. You might also specify the
-    // `conflictAlgorithm` to use in case the same dog is inserted twice.
-    //
-    // In this case, replace any previous data.
-    await db.insert(
-      'dogs',
-      dog.toMap(),
-      conflictAlgorithm: ConflictAlgorithm.replace,
-    );
-  }
-
-  // A method that retrieves all the dogs from the dogs table.
-  Future<List<Dog>> dogs() async {
-    // Get a reference to the database.
-    final db = await database;
-
-    // Query the table for all The Dogs.
-    final List<Map<String, dynamic>> maps = await db.query('dogs');
-
-    // Convert the List<Map<String, dynamic> into a List<Dog>.
-    return List.generate(maps.length, (i) {
-      return Dog(
-        id: maps[i]['id'],
-        name: maps[i]['name'],
-        age: maps[i]['age'],
-      );
-    });
-  }
-
-  Future<void> updateDog(Dog dog) async {
-    // Get a reference to the database.
-    final db = await database;
-
-    // Update the given Dog.
-    await db.update(
-      'dogs',
-      dog.toMap(),
-      // Ensure that the Dog has a matching id.
-      where: 'id = ?',
-      // Pass the Dog's id as a whereArg to prevent SQL injection.
-      whereArgs: [dog.id],
-    );
-  }
-
-  Future<void> deleteDog(int id) async {
-    // Get a reference to the database.
-    final db = await database;
-
-    // Remove the Dog from the database.
-    await db.delete(
-      'dogs',
-      // Use a `where` clause to delete a specific dog.
-      where: 'id = ?',
-      // Pass the Dog's id as a whereArg to prevent SQL injection.
-      whereArgs: [id],
-    );
-  }
+  }, version: 1);
 
   // Create a Dog and add it to the dogs table
   var fido = Dog(
@@ -104,14 +43,14 @@ void main() async {
     age: 35,
   );
 
-  await insertDog(fido);
+  int _lastId = await insertDog(fido);
 
   // Now, use the method above to retrieve all the dogs.
   print(await dogs()); // Prints a list that include Fido.
 
   // Update Fido's age and save it to the database.
   fido = Dog(
-    id: fido.id,
+    id: _lastId,
     name: fido.name,
     age: fido.age + 7,
   );
@@ -121,8 +60,75 @@ void main() async {
   print(await dogs()); // Prints Fido with age 42.
 
   // Delete Fido from the database.
-  await deleteDog(fido.id!);
+  // await deleteDog(fido.id!);
+  //
+  // // Print the list of dogs (empty).
+  // print(await dogs());
+}
 
-  // Print the list of dogs (empty).
-  print(await dogs());
+// Define a function that inserts dogs into the database
+Future<int> insertDog(Dog dog) async {
+  // Get a reference to the database.
+  final db = await database;
+
+  // Insert the Dog into the correct table. You might also specify the
+  // `conflictAlgorithm` to use in case the same dog is inserted twice.
+  //
+  // In this case, replace any previous data.
+  // await db.insert(
+  //   'dogs',
+  //   dog.toMap(),
+  //   conflictAlgorithm: ConflictAlgorithm.replace,
+  // );
+
+  return await db.rawInsert('INSERT INTO dogs (NAME,AGE) VALUES(\'${dog.name}\',${dog.age})');
+}
+
+// A method that retrieves all the dogs from the dogs table.
+Future<List<Dog>> dogs() async {
+  // Get a reference to the database.
+  final db = await database;
+
+  // Query the table for all The Dogs.
+  final List<Map<String, dynamic>> maps = await db.query('dogs');
+
+  // Convert the List<Map<String, dynamic> into a List<Dog>.
+  return List.generate(maps.length, (i) {
+    return Dog(
+      id: maps[i]['id'],
+      name: maps[i]['name'],
+      age: maps[i]['age'],
+    );
+  });
+}
+
+Future<void> updateDog(Dog dog) async {
+  // Get a reference to the database.
+  final db = await database;
+
+  // Update the given Dog.
+  // await db.update(
+  //   'dogs',
+  //   dog.toMap(),
+  //   // Ensure that the Dog has a matching id.
+  //   where: 'id = ?',
+  //   // Pass the Dog's id as a whereArg to prevent SQL injection.
+  //   whereArgs: [dog.id],
+  // );
+  
+  await db.rawUpdate('UPDATE dogs SET name = \'oscar\', age = 6 WHERE id = ${dog.id}');
+}
+
+Future<void> deleteDog(int id) async {
+  // Get a reference to the database.
+  final db = await database;
+
+  // Remove the Dog from the database.
+  await db.delete(
+    'dogs',
+    // Use a `where` clause to delete a specific dog.
+    where: 'id = ?',
+    // Pass the Dog's id as a whereArg to prevent SQL injection.
+    whereArgs: [id],
+  );
 }
